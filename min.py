c0853d03e5cc609000d2bf222f891c0643a0a848
George Kantasis <kantasisg@gmail.com>
2025-10-23 20:18:22 +0300

    write the min.py script

---
+++ min.py
@@ -0,0 +1,16 @@
+
+list_of_elements = [100,200,3124,15,35,13,6534,67,357,235,13,524,222,234,246,23,51,267]
+
+temp=list_of_elements[0]
+summ = 0
+
+#for i in range(len(list_of_elements)):
+for element in list_of_elements:
+   summ = summ + element
+
+
+
+print("Sum is",summ)
+print("Average is",summ / len(list_of_elements ) )
+
+
