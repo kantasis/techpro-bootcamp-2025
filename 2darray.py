cc9413266874963dedde193e6d7f15d95c6a70e9
George Kantasis <kantasisg@gmail.com>
2025-10-27 19:58:00 +0200

    adding 2d array traversal

---
+++ 2darray.py
@@ -0,0 +1,20 @@
+
+table = [
+   [ 1, 2, 3],
+   [ 4, 5, 6],
+   [ 7, 8, 9],
+   [10,11,12]
+]
+
+#print(table)
+
+for row in table:
+   for value in row:
+      print(value,' ' , end='')
+   print()
+# for i in range(len(table)):
+#    for j in range(len(table[i])):
+
+
+
+
